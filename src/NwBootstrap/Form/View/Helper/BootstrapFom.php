f41326bc4856ef32ecacb9094f3c78377d9b69b2
Renato Moura <renato@naturalweb.com.br>
2013-03-06 12:38:54 -0300

    Alterado bootstrapForm, para agir como o view heloer form original sem nenhum argumento retorna ele mesmo Adicionado os metodos: openTag, closeTag com estilos

--- src/NwBootstrap/Form/View/Helper/BootstrapFom.php
+++ src/NwBootstrap/Form/View/Helper/BootstrapFom.php
@@ -2,33 +2,70 @@
 namespace NwBootstrap\Form\View\Helper;
 
 use Zend\View\Helper\AbstractHelper;
-use Zend\Form\Form;
-use Zend\Form\Fieldset;
+use Zend\Form\FormInterface;
+use Zend\Form\FieldsetInterface;
 
 class BootstrapForm extends AbstractHelper
 {
-    public function __invoke(Form $form, $style = 'vertical')
+    public function __invoke(FormInterface $form, $style = 'vertical')
     { 
-        if ($style) {
-            $form->setAttribute('class', $form->getAttribute('class') . ' form-' . $style);
+        if (!$form) {
+            return $this;
         }
-        $form->prepare();
         
+        return $this->render($form, $style);
+    }
+    
+    /**
+     * Render a form from the provided $form,
+     *
+     * @param  ElementInterface          $element
+     * @param  null|string               $buttonContent
+     * @throws Exception\DomainException
+     * @return string
+     */
+    public function render(FormInterface $form, $style = 'vertical')
+    {
         $output = '';
         
-        $output .= $this->view->form()->openTag($form);
+        $output .= $this->openTag($form, $style);
         
-        $elements = $form->getIterator();
-        foreach ($elements as $key => $element) {
-            if ($element instanceof Fieldset) {
+        foreach ($form => $element) {
+            if ($element instanceof FieldsetInterface) {
                 $output .= $this->view->bootstrapCollection($element, $style);
             } else {
                 $output .= $this->view->bootstrapRow($element, $style);
             }
         }
         
-        $output .= $this->view->form()->closeTag($form);
+        $output .= $this->closeTag();
         
         return $output;
+    }
+    
+    /**
+     * Generate an opening form tag
+     *
+     * @param  null|FormInterface $form
+     * @return string
+     */
+    public function openTag(FormInterface $form = null, $style = 'vertical')
+    {
+        if ($style) {
+            $form->setAttribute('class', $form->getAttribute('class') . ' form-' . $style);
+        }
+        $form->prepare();
+        
+        return $this->view->form()->openTag($form);
+    }
+    
+    /**
+     * Generate a closing form tag
+     *
+     * @return string
+     */
+    public function closeTag()
+    {
+        return $this->view->form()->closeTag();
     }
 }
